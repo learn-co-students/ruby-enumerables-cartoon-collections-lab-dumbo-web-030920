1ad7c6ca27df0a6b87a33b8e4eeb3aa6404b73e4
Xavier Carty <xaviercarty@gmail.com>
2020-01-05 02:23:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf)# code an argument here
   # Your code here
+  dwarf.map{ |pair| p pair}
+end
 end
 
 def summon_captain_planet# code an argument here
